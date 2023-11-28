d86797421904784253558ed8ddb2da98092bdf4e
rgonzo <rgonzo1355@gmail.com>
2023-11-28 11:05:20 -0700

    first commit

---
+++ Challenge2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script Name:                  Arrays 
+# Author:                       Rodolfo Gonzalez
+# Date of latest revision:      10/29/2023
+# Purpose:                      Ops Challenge 5 
