5db7c7d5d737902c9d561080809f9ccea091983a
rgonzo <rgonzo1355@gmail.com>
2023-11-29 23:40:28 -0700

    first commit

---
+++ Challenge2b.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge 2 
+# Author:                       Rodolfo Gonzalez
+# Date of latest revision:      11/29/2023
+# Purpose:                      
+
+# Create a file to test with
+echo "This is my test file
---
+++ Challenge3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge 3 
+# Author:                       Rodolfo Gonzalez
+# Date of latest revision:      11/29/2023
+# Purpose:              
