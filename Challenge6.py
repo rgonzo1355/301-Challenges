bb9b6d76adba9aabd1845f85b35f0da7083f093f
rgonzo <rgonzo1355@gmail.com>
2023-12-04 17:21:10 -0700

    Topology A

---
+++ Challenge6.py
@@ -0,0 +1,33 @@
+# Author:       Rodolfo Gonzalez
+# Script        301 Ops Challenge 6
+# Purpose:      See python Example
+# Why           New Language
+
+# Variables
+
+# user = "Roger"
+
+# Output the initial value of User
+# print(user)
+
+# Input from the user
+# user = input("Please, enter your name: ")
+
+# Output the updated value of user
+
+print(user)
+
+
+#Inports
+os.system("ls ipconfig")
+
+
+# Bash Things in Python
+
+# Functions
+def my_function():
+    os.system("ipconfig")
+
+my_function()
+
+# Strech: If statement
